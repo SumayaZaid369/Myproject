f657100a76a0f49c14af2ce7b99c8da03850db90
SumayaZaid369 <somayazaid121@gmail.com>
2021-12-21 06:16:26 +0300

    finesh git and host

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,34 @@
-const express=require('express')
+const express =require('express')
 const app=express()
 
 
 const db =require('./db')
 const Todo =require('./todo')
-console.log (Todo)
+//console.log (Todo)
+
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.json('get is working ')
 
-})
+});
 
 app.get('/tasks',(req,res)=>{
-    res.json('get is working ')
+});
 
-})
+app.post('/tasks',(req,res)=>{
+    console.log('25:',req.body);
+
+  Todo.create(req.body,(err,newtask)=>{
+        if (err){
+            console.log("ERROR : ",err);
+
+      }
+     else {
+            res.status(201).json(newtask);
+       }
+    });
+});
 
 app.listen(5000,()=>{
     console.log('server working')
