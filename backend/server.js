f10e2767da5e0b66be8d4416dd9a0ba92483b4ab
SumayaZaid369 <somayazaid121@gmail.com>
2021-12-22 04:25:54 +0300

    finish put and delete

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,23 @@ app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.json('get is working ')
-
 });
+app.get("/tasks", (req, res) => {
+    Todo.find({}, (err, data) => {
+      if (err) {
+        console.log("ERROR: ", err);
+      } else {
+        res.json(data);
+      }
+    });
+  });
 
-app.get('/tasks',(req,res)=>{
-});
+
 
 app.post('/tasks',(req,res)=>{
     console.log('25:',req.body);
-
-  Todo.create(req.body,(err,newtask)=>{
+})  
+  /*Todo.create(req.body,(err,newtask)=>{
         if (err){
             console.log("ERROR : ",err);
 
@@ -28,7 +35,42 @@ app.post('/tasks',(req,res)=>{
             res.status(201).json(newtask);
        }
     });
+});*/
+app.put("/tasks/:id", (req, res) => {
+    // console.log("37:", req.params.id);
+    Todo.updateOne(
+        { _id: req.params.id },
+        { title: req.body.newTitle },
+        (err, updateObj) => {
+          if (err) {
+             console.log("ERROR: ", err);
+           res.status(400).json(err);
+          } else {
+            console.log(updateObj);
+            updateObj.modifiedCount === 1
+              ? res.json("Update one todo successfully")
+              : res.status(404).json("This todo is not found");
+          }
+        }
+      );
+    });
+
+
+app.delete("/tasks/:id", (req, res) => {
+    console.log("37:", req.params.id);
+
+Todo.deleteOne({ _id: req.params.id }, (err, deleteObj) => {
+    if (err) {
+      console.log("ERROR: ", err);
+    } else {
+
+        deleteObj.deletedCount === 1
+        ? res.json("Delete one todo successfully")
+        : res.status(404).json("This todo is not found");
+    }
+  });
 });
+
 
 app.listen(5000,()=>{
     console.log('server working')
